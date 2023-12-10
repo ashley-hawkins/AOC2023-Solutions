f30a1eb2933664708a5db87d683d3b9d3314ad2a
Ashley Hawkins <awhawkins@proton.me>
2023-12-10 13:53:30 +0000

    Day 1

--- 1/main.cc
+++ 1/main.cc
@@ -1,3 +1,25 @@
+#include <algorithm>
+#include <cinttypes>
+#include <iostream>
+
 int main()
 {
+	std::string line;
+	std::int32_t total{};
+	while (std::getline(std::cin, line))
+	{
+		auto pred = [](const auto &x) { return '0' <= x && x <= '9'; };
+		auto first = std::find_if(line.cbegin(), line.cend(), pred);
+		auto last = std::find_if(line.crbegin(), line.crend(), pred);
+
+		if (first != line.cend())
+		{
+			total += (*first - '0') * 10;
+		}
+		if (last != line.crend())
+		{
+			total += (*last - '0');
+		}
+	}
+	std::cout << total << "\n";
 }
